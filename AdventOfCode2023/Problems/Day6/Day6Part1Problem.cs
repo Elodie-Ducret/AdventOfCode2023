3cf47f7797e0290db7fedf3dbdfa969fe1184550
Elodie <elodie.s.ducret@gmail.com>
2023-12-06 11:24:57 +0100

    Day 6 part 1

---
+++ AdventOfCode2023/Problems/Day6/Day6Part1Problem.cs
@@ -0,0 +1,49 @@
+﻿using AdventOfCode2023.Common;
+using AdventOfCode2023.Schema.Day6;
+
+namespace AdventOfCode2023.Problems.Day6;
+
+public class Day6Part1Problem(string name, string path) : Problem<List<Race>, int>(name, path)
+{
+    public static void Run()
+    {
+        var day1Problem = new Day6Part1Problem("Day 6 Part 1", "Day6/Day6.txt");
+        var response = day1Problem.SolveProblem();
+    }
+
+    public static void RunTest()
+    {
+        var day1Problem = new Day6Part1Problem("Day 6 Part 1 Test", "Day6/Day6_Part1_Test.txt");
+        var response = day1Problem.SolveProblem();
+    }
+
+
+    protected override List<Race> Convert(IEnumerable<string> input)
+    {
+        var times = input.First();
+        var timesSplit = times.Split(": ")[1].Split(" ").Where(x => x != string.Empty).ToList();
+        var distances = input.Last();
+        var distancesSplit = distances.Split(": ")[1].Split(" ").Where(x => x != string.Empty).ToList();
+
+
+        var list = timesSplit.Select((t, i) => new Race(int.Parse(t), int.Parse(distancesSplit[i]))).ToList();
+        return list;
+    }
+
+    protected override int Solve(List<Race> input)
+    {
+        var response = input.Select(GetWinCount).Aggregate(1, (x, y) => x * y);
+        return response;
+    }
+
+
+    private int GetWinCount(Race race)
+    {
+        var sum = 0;
+        for (var i = 0; i < race.Time; i++)
+        {
+            if (race.IsWin(i)) sum++; 
+        }
+        return sum; 
+    }
+}
---
+++ AdventOfCode2023/Schema/Day6/Race.cs
@@ -0,0 +1,17 @@
+﻿namespace AdventOfCode2023.Schema.Day6;
+
+public record Race(int Time, int Distance)
+{
+    public int Time = Time;
+    public int Distance = Distance;
+    
+    public int GetDistanceTraveled(int pressDuration)
+    {
+        return (Time - pressDuration) * pressDuration;
+    }
+
+    public bool IsWin(int pressDuration)
+    {
+        return GetDistanceTraveled(pressDuration) > Distance;
+    }
+}
