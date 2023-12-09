c67cc961595bbc66ee96b394a30fc37a6722e85b
Elodie <elodie.s.ducret@gmail.com>
2023-12-09 15:19:04 +0100

    Day 9 part 1

---
+++ AdventOfCode2023/Schema/Day9/History.cs
@@ -0,0 +1,34 @@
+﻿using System.Transactions;
+
+namespace AdventOfCode2023.Schema.Day9;
+
+public record History(List<int> Values)
+{
+    public List<int> Values = Values;
+    private List<List<int>>? _pyramid;
+
+    public int GetNext()
+    {
+        _pyramid = new List<List<int>> { Values };
+        var precedent = _pyramid[0];
+        while (precedent.Any(x => x != 0))
+        {
+            var list = new List<int>();
+            for (int i = 1; i < precedent.Count; i++)
+            {
+                list.Add(precedent[i] - precedent[i - 1]);
+            }
+
+            _pyramid.Add(list);
+            precedent = _pyramid.Last();
+        }
+        
+        _pyramid.Last().Add(0);
+        for (var i = _pyramid.Count-2; i >= 0; i--)
+        {
+            _pyramid[i].Add(_pyramid[i+1].Last() + _pyramid[i].Last());
+        }
+
+        return _pyramid.First().Last();
+    }
+}
---
+++ AdventOfCode2023/Schema/Day9/Oasis.cs
@@ -0,0 +1,6 @@
+﻿namespace AdventOfCode2023.Schema.Day9;
+
+public record Oasis(List<History> Histories)
+{
+    public List<History> Histories = Histories;
+}
