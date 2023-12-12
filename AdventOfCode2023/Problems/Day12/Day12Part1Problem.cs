4070a2cdb8b123508b89163fa1f1b142dc233941
Elodie <elodie.s.ducret@gmail.com>
2023-12-12 16:13:54 +0100

    Day 12 part 1

---
+++ AdventOfCode2023/Problems/Day12/Day12Part1Problem.cs
@@ -0,0 +1,38 @@
+﻿using AdventOfCode2023.Common;
+using AdventOfCode2023.Schema.Day12;
+
+namespace AdventOfCode2023.Problems.Day12;
+
+public class Day12Part1Problem(string name, string path) : Problem<List<SpringsRecord>, long>(name, path)
+{
+    public static void Run()
+    {
+        var day1Problem = new Day12Part1Problem("Day 12 Part 1", "Day12/Day12.txt");
+        var response = day1Problem.SolveProblem();
+    }
+
+    public static void RunTest1()
+    {
+        var day1Problem = new Day12Part1Problem("Day 12 Part 1 Test1", "Day12/Day12_Part1_Test1.txt");
+        var response = day1Problem.SolveProblem();
+    }
+
+    protected override List<SpringsRecord> Convert(IEnumerable<string> input)
+    {
+        var list = new List<SpringsRecord>();
+        foreach (var row in input)
+        {
+            var rowSplit = row.Split(' ');
+            var springs = rowSplit[0];
+            var conditions = rowSplit[1].Split(',').Select(int.Parse).ToList();
+            list.Add(new SpringsRecord([..springs], conditions));
+        }
+
+        return list;
+    }
+
+    protected override long Solve(List<SpringsRecord> input)
+    {
+        return input.Sum(x=> x.GetArrangementValidCount());
+    }
+}
---
+++ AdventOfCode2023/Schema/Day12/SpringsRecord.cs
@@ -0,0 +1,56 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2023.Schema.Day12;
+
+public record SpringsRecord(List<char> Springs, List<int> Conditions)
+{
+    public List<char> Springs = Springs;
+    public List<int> Conditions = Conditions;
+
+
+    private List<List<char>> Possibilities = new();
+
+    private readonly Regex _regex = new Regex(@"(?:\#+)|\.+");
+
+
+    public long GetArrangementValidCount()
+    {
+        GetAllPossibilities(Springs);
+        return Possibilities.Count; 
+    }
+
+
+    private void GetAllPossibilities(List<char> inputString)
+    {
+        if (inputString.Contains('?'))
+        {
+            List<char> newList = [..inputString];
+            var index = newList.IndexOf('?');
+            newList[index] = '#';
+            GetAllPossibilities(newList);
+            newList[index] = '.';
+            GetAllPossibilities(newList);
+        }
+        else
+        {
+            if (IsValid(inputString))
+            {
+                Possibilities.Add(inputString);
+            }
+        }
+    }
+
+    private bool IsValid(List<char> input)
+    {
+        var list = new List<int>();
+        var matches = _regex.Matches(string.Join("", input));
+        foreach (Match match in matches)
+        {
+            var m = match.Groups[0].Value;
+            if(m[0] == '#') list.Add(m.Length);
+        }
+
+        if (list.Count != Conditions.Count) return false;
+        return !list.Where((t, i) => t != Conditions[i]).Any();
+    }
+}
