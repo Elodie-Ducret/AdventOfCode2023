f5ad228cf1c5b13b9730695881ab64ac23da29f3
Elodie <elodie.s.ducret@gmail.com>
2023-12-13 17:45:55 +0100

    Day 13 part 1

---
+++ AdventOfCode2023/Problems/Day13/Day13Part1Problem.cs
@@ -0,0 +1,85 @@
+﻿using AdventOfCode2023.Common;
+using AdventOfCode2023.Schema.Day13;
+
+namespace AdventOfCode2023.Problems.Day13;
+
+public class Day13Part1Problem(string name, string path) : Problem<List<Pattern>, long>(name, path)
+{
+    public static void Run()
+    {
+        var day1Problem = new Day13Part1Problem("Day 13 Part 1", "Day13/Day13.txt");
+        var response = day1Problem.SolveProblem();
+    }
+
+    public static void RunTest1()
+    {
+        var day1Problem = new Day13Part1Problem("Day 13 Part 1 Test1", "Day13/Day13_Part1_Test1.txt");
+        var response = day1Problem.SolveProblem();
+    }
+    
+    public static void RunTest2()
+    {
+        var day1Problem = new Day13Part1Problem("Day 13 Part 1 Test2", "Day13/Day13_Part1_Test2.txt");
+        var response = day1Problem.SolveProblem();
+    }
+
+    protected override List<Pattern> Convert(IEnumerable<string> input)
+    {
+        var fullInput = string.Join('\n', input);
+
+        var splitInput = fullInput.Split("\n\n");
+
+        var patterns = new List<Pattern>();
+        foreach (var singlePattern in splitInput)
+        {
+            var pattern = singlePattern.Split('\n');
+
+            var columnCount = pattern.First().Length;
+            var rowCount = pattern.Length;
+            var list = new bool[rowCount, columnCount];
+
+            var count = 0;
+            foreach (var line in pattern)
+            {
+                for (int i = 0; i < columnCount; i++)
+                {
+                    list[count, i] = line[i] == '#';
+                }
+
+                count++;
+            }
+
+            patterns.Add(new Pattern(list, rowCount, columnCount));
+        }
+
+
+        return patterns;
+    }
+
+    protected override long Solve(List<Pattern> input)
+    {
+        var sum = 0;
+        foreach (var pattern in input)
+        {
+            Console.WriteLine("--------------------------");
+            pattern.PrintPattern();
+            var sumNumberOfRowAboveHorizontal = 0;
+            for (int row = 0; row < pattern.RowCount; row++)
+            {
+                sumNumberOfRowAboveHorizontal += pattern.GetMirrorRowCount(row);
+            }
+
+
+            var sumNumberOfColumnLeftVertical = 0;
+            for (int column = 0; column < pattern.ColumnCount; column++)
+            {
+                sumNumberOfColumnLeftVertical += pattern.GetMirrorColumnCount(column);
+            }
+
+            sum += sumNumberOfColumnLeftVertical + 100 * sumNumberOfRowAboveHorizontal;
+            Console.WriteLine("--------------------------");
+        }
+
+        return sum;
+    }
+}
---
+++ AdventOfCode2023/Schema/Day13/Pattern.cs
@@ -0,0 +1,56 @@
+﻿namespace AdventOfCode2023.Schema.Day13;
+
+public record Pattern(bool[,] Floor, int RowCount, int ColumnCount)
+{
+    public bool[,] Floor = Floor;
+    public int RowCount = RowCount;
+    public int ColumnCount = ColumnCount;
+
+
+    public void PrintPattern()
+    {
+        for (int row = 0; row < RowCount; row++)
+        {
+            for (int column = 0; column < ColumnCount; column++)
+            {
+                Console.Write(Floor[row, column] ? '#' : '.');
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+    public int GetMirrorRowCount(int row)
+    {
+        var count = Math.Min(row + 1, RowCount - 1 - row);
+        if (count <= 0) return 0;
+        for (var rowFloor = 0; rowFloor < count; rowFloor++)
+        {
+            for (int columnFloor = 0; columnFloor < ColumnCount; columnFloor++)
+            {
+                if (Floor[row - rowFloor, columnFloor] != Floor[row + 1 + rowFloor, columnFloor]) return 0;
+            }
+        }
+
+        if (count == 0) return 0;
+        Console.WriteLine($"ROW OK : `{row}` => `{row + 1}`");
+        return row + 1;
+    }
+
+    public int GetMirrorColumnCount(int column)
+    {
+        var count = Math.Min(column + 1, ColumnCount - 1 - column);
+        if (count <= 0) return 0;
+        for (var columnFloor = 0; columnFloor < count; columnFloor++)
+        {
+            for (int rowFloor = 0; rowFloor < RowCount; rowFloor++)
+            {
+                if (Floor[rowFloor, column - columnFloor] != Floor[rowFloor, column + 1 + columnFloor]) return 0;
+            }
+        }
+
+        if (count == 0) return 0;
+        Console.WriteLine($"COLUMN OK : `{column}`=> `{column + 1}`");
+        return column + 1;
+    }
+}
