07fd7363ceb1fe021612ebc83e1fcd5062bd08b2
Elodie <elodie.s.ducret@gmail.com>
2024-01-19 11:05:54 +0100

    Update Position.cs

--- AdventOfCode2023/Common/Position.cs
+++ AdventOfCode2023/Common/Position.cs
@@ -1,9 +1,9 @@
 namespace AdventOfCode2023.Common;
 
-public record struct Position(int Row, int Column)
+public record struct Position(long Row, long Column)
 {
-    public int Row = Row;
-    public int Column = Column;
+    public long Row = Row;
+    public long Column = Column;
 
     public static Position operator +(in Position lhs, in Position rhs) =>
         new(lhs.Row + rhs.Row,
@@ -14,7 +14,7 @@ public record struct Position(int Row, int Column)
         new(lhs.Row - rhs.Row,
             lhs.Column - rhs.Column);
 
-    public static Position operator *(in int lhs, in Position rhs) =>
+    public static Position operator *(in long lhs, in Position rhs) =>
         new(lhs * rhs.Row,
             lhs * rhs.Column);
 }
