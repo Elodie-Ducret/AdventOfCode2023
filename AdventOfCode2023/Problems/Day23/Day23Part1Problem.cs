379d74788cc27f215ef16a137d168ad627886f47
Elodie <elodie.s.ducret@gmail.com>
2024-01-19 11:03:19 +0100

    Day 23 Part 1

---
+++ AdventOfCode2023/Problems/Day23/Day23Part1Problem.cs
@@ -0,0 +1,52 @@
+﻿using AdventOfCode2023.Common;
+using AdventOfCode2023.Schema.Day23;
+
+namespace AdventOfCode2023.Problems.Day23;
+
+public class Day23Part1Problem(string name, string path) : Problem<Graph, long>(name, path)
+{
+    public static void Run()
+    {
+        var problem = new Day23Part1Problem("Day 23 Part 1", "Day23/Day23.txt");
+        var response = problem.SolveProblem();
+    }
+
+    public static void RunTest1()
+    {
+        var problem = new Day23Part1Problem("Day 23 Part 1 Test1", "Day23/Day23_Part1_Test1.txt");
+        var response = problem.SolveProblem();
+    }
+
+    protected override Graph Convert(IEnumerable<string> input)
+    {
+        var fullInput = string.Join('\n', input);
+        var lines = fullInput.Split('\n');
+
+        var columnCount = lines.First().Length;
+        var rowCount = lines.Length;
+        var titles = new char[rowCount, columnCount];
+        var start = new Position();
+        var end = new Position();
+
+        var count = 0;
+        foreach (var line in lines)
+        {
+            if (count == 0) start = new Position(0, line.IndexOf('.'));
+            if (count == columnCount - 1) end = new Position(columnCount - 1, line.IndexOf('.'));
+
+            for (var i = 0; i < columnCount; i++)
+            {
+                titles[count, i] = line[i];
+            }
+
+            count++;
+        }
+
+        return new Graph(titles, rowCount, columnCount, start, end);
+    }
+
+    protected override long Solve(Graph input)
+    {
+        return input.GetMaxCount();
+    }
+}
---
+++ AdventOfCode2023/Schema/Day23/Graph.cs
@@ -0,0 +1,71 @@
+﻿using AdventOfCode2023.Common;
+
+namespace AdventOfCode2023.Schema.Day23;
+
+public record Graph(char[,] Tiles, int RowCount, int ColumnCount, Position Start, Position End)
+{
+    private readonly HashSet<Position> _explored = new();
+    private long _maxDistance = -1;
+
+    public long GetMaxCount()
+    {
+        Visit(Start, 0);
+        return _maxDistance;
+    }
+
+    private void Visit(Position node, long distance)
+    {
+        _explored.Add(node);
+        if (node == End)
+        {
+            if (distance > _maxDistance) _maxDistance = distance;
+            _explored.Remove(node);
+            return;
+        }
+
+        foreach (var neighbor in GetNeighbors(node).Where(neighbor => !_explored.Contains(neighbor.position)))
+        {
+            Visit(neighbor.position, distance + neighbor.distance);
+        }
+
+        _explored.Remove(node);
+    }
+
+    private List<(Position position, int distance)> GetNeighbors(Position position)
+    {
+        var neighbors = new List<(Position position, int distance)>();
+        foreach (Direction direction in Enum.GetValues(typeof(Direction)))
+        {
+            var nextPosition = position + DirectionPosition.GetPosition(direction);
+            if (nextPosition.Row >= 0 && nextPosition.Row < RowCount && nextPosition.Column >= 0 &&
+                nextPosition.Column < ColumnCount)
+            {
+                switch (Tiles[nextPosition.Row, nextPosition.Column])
+                {
+                    case '#':
+                        break;
+                    case '.':
+                        neighbors.Add((nextPosition, 1));
+                        break;
+                    default:
+                        var dir = GetDirection(Tiles[nextPosition.Row, nextPosition.Column]);
+                        neighbors.Add((nextPosition + DirectionPosition.GetPosition(dir), 2));
+                        break;
+                }
+            }
+        }
+
+        return neighbors.ToList();
+    }
+
+    private static Direction GetDirection(char c)
+    {
+        return c switch
+        {
+            '<' => Direction.West,
+            'v' => Direction.South,
+            '>' => Direction.East,
+            _ => throw new ArgumentException($"Unknown {c}")
+        };
+    }
+}
